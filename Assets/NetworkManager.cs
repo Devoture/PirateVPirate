7ea6bf859dd5c90eb9d6198335b05f75d27e2d94
Tom Sauer <tom@exluto.com>
2018-02-24 13:29:28 -0500

    working on NM

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkManager_Custom : NetworkManager {
+
+	public void StartupHost() {
+		SetPort();
+		NetworkManager.singleton.StartHost();
+	}
+	public void JoinGame() {
+		SetIPAdress();
+		Setport();
+		NetworkManager.singleton.StartClient();
+	}
+	void SetIPAdress() {
+		string ipAdress = GameObject.Find("InputFieldAdress").transform.Find("text").GetComponnet<text>().text;
+		NetworkManager.singleton.networkAdress = ipAdress;
+	}
+	void SetPort() {
+		NetworkManager.singleton.networkPort = 7777;
+	}
+}
