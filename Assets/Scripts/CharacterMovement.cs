3657ab3368ca9e862b0e4e93806aff800fb9dc1f
Maaz <Maaz_haq123@hotmail.com>
2018-02-25 12:32:22 -0500

    added blocking animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -18,6 +18,7 @@ public class CharacterMovement : NetworkBehaviour {
 	private SwordCollider m_swordColliderScript;
 	private bool m_isAttacking;
 	private Health m_healthScript;
+	private bool m_disableMovement;
 
 	// Use this for initialization
 	void Start() {
@@ -30,30 +31,43 @@ public class CharacterMovement : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if(!m_disableMovement) {
+			m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-		if(Input.GetButtonDown("Jump") && m_isGrounded) {
-			m_moveDirection.y = m_jumpSpeed;
-			m_isJumping = true;
+			if(Input.GetButtonDown("Jump") && m_isGrounded) {
+				m_moveDirection.y = m_jumpSpeed;
+				m_isJumping = true;
+			}
+
+			m_moveDirection *= m_speed * m_speedMultiplier;
+
+			m_animController.SetFloat("Forward", m_moveDirection.z);
+			m_animController.SetFloat("Right", m_moveDirection.x);
+
+			m_moveDirection = transform.TransformDirection(m_moveDirection);
+			
+
+			m_moveDirection.y -= m_gravity * Time.deltaTime;
+			m_isGrounded = ((m_controller.Move(m_moveDirection * Time.deltaTime)) & CollisionFlags.Below) != 0;
+
+
+			if(Input.GetMouseButtonDown(0) && m_isAttacking == false) {
+				m_swordCollider.enabled = true;			
+				m_animController.SetBool("isAttacking", true);
+				m_isAttacking = true;
+				Debug.Log(m_animController.GetBool("isAttacking"));
+			}
 		}
 
-		m_moveDirection *= m_speed * m_speedMultiplier;
-
-		m_animController.SetFloat("Forward", m_moveDirection.z);
-		m_animController.SetFloat("Right", m_moveDirection.x);
-
-		m_moveDirection = transform.TransformDirection(m_moveDirection);
-		
-
-		m_moveDirection.y -= m_gravity * Time.deltaTime;
-		m_isGrounded = ((m_controller.Move(m_moveDirection * Time.deltaTime)) & CollisionFlags.Below) != 0;
-
-
-		if(Input.GetMouseButtonDown(0) && m_isAttacking == false) {
-			m_swordCollider.enabled = true;			
-			m_animController.SetBool("isAttacking", true);
-			m_isAttacking = true;
-			Debug.Log(m_animController.GetBool("isAttacking"));
+		if(Input.GetMouseButtonDown(1)) {
+			m_disableMovement = true;
+			m_animController.SetBool("isBlocking", true);
+			m_healthScript.m_cantTakeDamage = true;
+		}
+		if(Input.GetMouseButtonUp(1)) {
+			m_disableMovement = false;
+			m_animController.SetBool("isBlocking", false);
+			m_healthScript.m_cantTakeDamage = false;
 		}
 
 		if(Input.GetKeyDown(KeyCode.R)) {
