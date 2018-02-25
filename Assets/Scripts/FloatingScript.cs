91df2a0e783c53dabbde895e614db35a47ff3914
MaximumCarnage <dylansbruton@gmail.com>
2018-02-24 19:59:14 -0500

    start of floating script

---
+++ Assets/Scripts/FloatingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingScript : MonoBehaviour {
+private float speed = 10.0f;
+private float amplitude = 20f;
+private float y0;
+private float newY;
+
+	// Use this for initialization
+	void Start () {
+		 y0 = transform.position.y;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newY=y0+amplitude*Mathf.Sin(speed*Time.time);
+		//transform.position.y =newY;
+	}
+}
