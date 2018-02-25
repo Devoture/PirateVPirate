34108e7283a189d19b6f3c9c18c30025412d32c5
topleftstyll <joshua.brackett@hotmail.ca>
2018-02-25 03:28:29 -0500

    fixed maaz sword again

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class SwordCollider : MonoBehaviour {
 	private float m_damage = 10.0f;
 	private Animator m_animController;
-	private MeshCollider m_swordCollider;
 	private bool m_hasDealtDamage = false;
 
 
 	// Use this for initialization
 	void Start () {
 		m_animController = transform.root.GetComponent<Animator>();
-		m_swordCollider = GetComponent<MeshCollider>();
 	}
 	
 	// Update is called once per frame
@@ -32,24 +30,14 @@ public class SwordCollider : MonoBehaviour {
 		}
 	}
 
-	public void StartAttack() {
-		m_swordCollider.enabled = true;
-	}
-
-	public void BlockAttack() {
-		m_swordCollider.enabled = true;
-	}
-
 	public void ResetAttack() {
 		m_animController.SetBool("isAttacking", false);
 		Debug.Log("sc " + m_animController.GetBool("isAttacking"));
-		m_swordCollider.enabled = false;
 		m_hasDealtDamage = false;
 	}
 
 	public void ResetBlock() {
 		m_animController.SetBool("isBlocking", false);
-		m_swordCollider.enabled = false;
 		m_hasDealtDamage = false;
 	}
 }
