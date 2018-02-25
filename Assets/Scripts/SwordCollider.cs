4df1fb2c0f95162480eea03250ac542e9fb9269a
Maaz <Maaz_haq123@hotmail.com>
2018-02-24 22:18:46 -0500

    added damage to the sword when it hits the player

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordCollider : MonoBehaviour {
+	private float m_damage = 10.0f;
 	private Animator m_animController;
 	private Collider m_swordCollider;
 	private bool m_hasDealtDamage = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +21,10 @@ public class SwordCollider : MonoBehaviour {
 	}
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Enemy" && !m_hasDealtDamage) {
-			m_hasDealtDamage = true;
+			if(other.GetComponent<Health>() != null) {
+				m_hasDealtDamage = true;
+				other.GetComponent<Health>().TakeDamage(m_damage);
+			}
 		}
 	}
 
