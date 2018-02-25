1c9bf4ffbb9292c4cf6bf7397dbb2331ae7c6c30
MaximumCarnage <dylansbruton@gmail.com>
2018-02-25 13:19:22 -0500

    Started Sound, added spotlight

---
+++ Assets/Scripts/SoundMGR.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMGR : MonoBehaviour {
+	public AudioClip m_oceanSfx;
+	public static SoundMGR Instance { get { return m_instance; } }
+	private static SoundMGR m_instance = null;
+	// Use this for initialization
+	void Start () {
+		if(m_instance != null && m_instance != this) {
+			Destroy(this.gameObject);
+			return; 
+		} else {
+			m_instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	
+}
