cf3a0a8dda19f5ac82925b50a12ae09179e71983
topleftstyll <joshua.brackett@hotmail.ca>
2018-02-25 13:37:25 -0500

    hopefully fixed sword blocking

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -10,23 +10,21 @@ public class SwordCollider : NetworkBehaviour {
 	private float m_damage = 10.0f;
 	private Animator m_animController;
 	private Health m_healthScript;
-	private CharacterMovement m_charScript;
 
 	void Start () {
 		m_animController = transform.root.GetComponent<Animator>();
 		m_healthScript = transform.root.GetComponent<Health>();
-		m_charScript = transform.root.GetComponent<CharacterMovement>();
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Enemy" && !m_hasDealtDamage) {
 			if(m_healthScript.m_cantTakeDamage) {
-				m_charScript.m_numOfBlockedAttacks++;
-				Debug.Log("Number OF Blocked Attacks: " + m_charScript.m_numOfBlockedAttacks);
+				other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks++;
+				Debug.Log("Number OF Blocked Attacks: " + other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks);
 				m_animController.SetBool("blockedAttack", true);
 			} 
-			if(other.GetComponent<Health>() != null && !m_healthScript.m_cantTakeDamage) {
-				m_charScript.m_numOfBlockedAttacks = 0;
+			if(other.GetComponent<Health>() != null && !other.GetComponent<Health>().m_cantTakeDamage) {
+				other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks = 0;
 				Debug.Log("Reset Blocked Number");
 				m_hasDealtDamage = true;
 				other.GetComponent<Health>().TakeDamage((int)m_damage);
