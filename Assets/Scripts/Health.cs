8ce01d221cde5430600d8d5c27570a2358c44113
topleftstyll <joshua.brackett@hotmail.ca>
2018-02-26 18:55:10 -0500

    added hud update when you take damage

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,15 +5,17 @@ using UnityEngine.Networking;
 public class Health : NetworkBehaviour {
 
     public const int maxHealth = 100;
+	public Image m_playerHUD;
 
     public int currHealth = maxHealth;
 
     public void TakeDamage(int amount) {
     	currHealth -= amount;
 		Debug.Log(currHealth);
+		UpdateHealth();
 	}
 
-	void UpdateHealth(int health) {
-		currHealth = health;
+	void UpdateHealth() {
+		m_playerHUD.fillAmount = (float)currHealth / (float)maxHealth;
 	}
 }
