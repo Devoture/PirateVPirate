94413cdc875af1f5cc07152d3399fdc68704f4a4
Maaz <Maaz_haq123@hotmail.com>
2018-02-26 17:19:03 -0500

    more health testing

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,14 +5,20 @@ using UnityEngine.Networking;
 public class Health : NetworkBehaviour {
 
     public const int maxHealth = 100;
+	private CharacterMovement m_charMovement;
 
     [SyncVar (hook = "UpdateHealth")]
     public int currHealth = maxHealth;
+
+	void Start() {
+		m_charMovement = GetComponent<CharacterMovement>();
+	}
 
     public void TakeDamage(int amount) {
     	if (isServer) {
         	currHealth -= amount;
     	} else {
+			m_charMovement.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
         	CmdTakeDamage (amount);
     	}
 	}
