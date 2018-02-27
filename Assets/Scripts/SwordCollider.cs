e292bf610d91592a294c13d39aba9b11831c6766
topleftstyll <joshua.brackett@hotmail.ca>
2018-02-26 19:02:31 -0500

    added a bunch of debugs

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -16,17 +16,22 @@ public class SwordCollider : NetworkBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
+		Debug.Log("hit something");
 		var hit = other.gameObject;
 		var hitPlayer = hit.GetComponent<Health>();
 		if(other.tag == "Enemy" && !m_hasDealtDamage) {
+			Debug.Log("hit enemy");
 			if(other.GetComponent<CharacterMovement>().m_animController.GetBool("isBlocking")) {
+				Debug.Log("hit blocked attack");
 				other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks++;
 				Debug.Log("Blocking..." + other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks);
 				Debug.Log("Number OF Blocked Attacks: " + other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks);
 				other.GetComponent<CharacterMovement>().m_animController.SetBool("blockedAttack", true);
 			} 
 			if(hitPlayer != null) {
+				Debug.Log("hit health");
 				if(other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks > 3) {
+					Debug.Log("block broke");
 					other.GetComponent<CharacterMovement>().m_animController.SetBool("blockedAttack", true);
 					other.GetComponent<CharacterMovement>().m_animController.SetBool("isBlocking", false);
 					other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks = 0;
