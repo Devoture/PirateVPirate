32ce16db70da761b3dbeacb8ce6e2a0211157260
topleftstyll <joshua.brackett@hotmail.ca>
2018-02-27 20:57:50 -0500

    sadf

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -31,7 +31,7 @@ public class SwordCollider : NetworkBehaviour {
 					other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks = 0;
 				}
 				if(!other.GetComponent<CharacterMovement>().m_animController.GetBool("isBlocking")) {
-					if(!hitPlayer.isServer) {
+					if(hitPlayer.isServer) {
 						hitPlayer.SetCurrentHealth(hitPlayer.GetCurrentHealth() - 10);
 					} else {
 						hitPlayer.TakeDamage(10);
