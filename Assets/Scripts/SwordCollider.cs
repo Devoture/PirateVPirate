439650da8406a98f48d7cf301bd1103a315e20ec
MaximumCarnage <dylansbruton@gmail.com>
2018-02-27 19:28:40 -0500

    got sword causing sound on hit/on block

--- Assets/Scripts/SwordCollider.cs
+++ Assets/Scripts/SwordCollider.cs
@@ -8,7 +8,16 @@ public class SwordCollider : MonoBehaviour {
 	private float m_damage = 10.0f;
 	private Animator m_animController;
 	private Health m_healthScript;
+	public SoundMGR m_soundmgr;
 
+	public AudioSource m_swordsource;
+	public AudioClip m_hit1;
+	public AudioClip m_hit2;
+	public AudioClip m_hit3;
+
+	public AudioClip m_clashhit1;
+	public AudioClip m_clashhit2;
+	public int m_randnum;
 	void Start () {
 		m_animController = transform.root.GetComponent<Animator>();
 		m_healthScript = transform.root.GetComponent<Health>();
@@ -29,6 +38,7 @@ public class SwordCollider : MonoBehaviour {
 					other.GetComponent<CharacterMovement>().m_numOfBlockedAttacks = 0;
 				}
 				if(!other.GetComponent<CharacterMovement>().m_animController.GetBool("isBlocking")) {
+					PlayerHit();
 					hitPlayer.TakeDamage(10);
 					Debug.Log("Take damage");
 					Debug.Log(hitPlayer.GetCurrentHealth());
@@ -38,7 +48,30 @@ public class SwordCollider : MonoBehaviour {
 			m_hasDealtDamage = true;
 		}
 	}
+	public void PlayerHit(){
+		m_randnum = Random.Range(1,3);
+		if(m_randnum == 1){
+			m_swordsource.PlayOneShot(m_hit1);
+		}
+		if(m_randnum == 2){
+			m_swordsource.PlayOneShot(m_hit2);
+		}
+		if(m_randnum == 3){
+			m_swordsource.PlayOneShot(m_hit3);
+		}
+		
+	}
 
+	public void blockhit(){
+		m_randnum = Random.Range(1,2);
+
+		if(m_randnum == 1){
+			m_swordsource.PlayOneShot(m_clashhit1);
+		}
+		if(m_randnum == 2){
+			m_swordsource.PlayOneShot(m_clashhit2);
+		}
+	}
 	public void ResetBlock() {
 		m_animController.SetBool("isBlocking", false);
 		m_hasDealtDamage = false;
