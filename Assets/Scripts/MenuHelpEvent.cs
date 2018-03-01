25f4e76041d7e6a0c261ed4281ef7350094df835
MaximumCarnage <dylansbruton@gmail.com>
2018-03-01 04:41:15 -0500

    Help Canvas

---
+++ Assets/Scripts/MenuHelpEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHelpEvent : MonoBehaviour {
+
+	public MenuStates m_menuStateMgr;
+	public void OnMouseDown(){
+		m_menuStateMgr.Help();
+	}
+}
