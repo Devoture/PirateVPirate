55d719a026a7718b2c0701b25c4e0d16d8d04842
Maaz <Maaz_haq123@hotmail.com>
2018-03-01 19:21:34 -0500

    asdsa

--- Assets/Scripts/HitCollider.cs
+++ Assets/Scripts/HitCollider.cs
@@ -30,7 +30,8 @@ public class HitCollider : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if(this.name == "Pirate1") {
-			if(other.name == "Sword" && (!other.transform.root.GetComponent<HitCollider>().m_hasDealtDamage && other.transform.root.name == "Enemy")) {
+			Debug.Log(other.transform.root.name);
+			if(other.name == "Sword" && (!other.transform.root.GetComponent<HitCollider>().m_hasDealtDamage && other.transform.root.name == "Pirate2")) {
 				if(GetComponent<CharacterMovement>().m_animController.GetBool("isBlocking")) {
 					m_swordAudSrc.PlayOneShot(m_clash1);
 					GetComponent<CharacterMovement>().m_numOfBlockedAttacks++;
@@ -48,7 +49,7 @@ public class HitCollider : MonoBehaviour {
 				}
 			}
 		} else if (this.name == "Pirate2") {
-			if(other.name == "Sword" && (!other.transform.root.GetComponent<HitCollider>().m_hasDealtDamage && other.transform.root.tag == "Enemy")) {
+			if(other.name == "Sword" && (!other.transform.root.GetComponent<HitCollider>().m_hasDealtDamage)) {
 				if(GetComponent<CharacterMovement>().m_animController.GetBool("isBlocking")) {
 					m_swordAudSrc.PlayOneShot(m_clash1);
 					GetComponent<CharacterMovement>().m_numOfBlockedAttacks++;
