ed80f76722e8430b09d7ff55f636b6726a057012
topleftstyll <joshua.brackett@hotmail.ca>
2018-03-02 19:40:54 -0500

    SADf

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -161,10 +161,7 @@ public class CharacterMovement : NetworkBehaviour {
 	}
 
 	public void GameOver() {
-		if(isLocalPlayer){
-			MatchInfo matchInfo = m_networkManager.matchInfo;
-			m_networkManager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, m_networkManager.OnDropConnection);
-			m_networkManager.StopHost();
+		if(isLocalPlayer) {
 			if(!m_isDead) {
 				// win scene
 				SceneManager.LoadScene("Win");
@@ -172,6 +169,9 @@ public class CharacterMovement : NetworkBehaviour {
 				// lose scene
 				SceneManager.LoadScene("Lose");
 			}
+			MatchInfo matchInfo = m_networkManager.matchInfo;
+			m_networkManager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, m_networkManager.OnDropConnection);
+			m_networkManager.StopHost();
 		}
 	}
 }
