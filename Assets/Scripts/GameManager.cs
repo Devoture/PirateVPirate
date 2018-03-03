e69f29d7bc9e9f6167dba17f0cee4d8c04808de9
topleftstyll <joshua.brackett@hotmail.ca>
2018-03-02 20:22:28 -0500

    asdf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,6 +58,7 @@ public class GameManager : MonoBehaviour {
 
 	public void CheckGameOver() {
 		for(int i = 0; i < m_players.Count; i++) {
+			Debug.Log("number of players: " + i);
 			m_players[i].GetComponent<CharacterMovement>().GameOver();
 		}
 	}
